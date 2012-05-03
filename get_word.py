4d795c7a1b32c8151eeb8db4be225a01506d7242
xiaomo <code.vvip@gmail.com>
2012-05-04 01:16:24 +0800

    version1

--- get_word.py
+++ get_word.py
@@ -1,7 +1,9 @@
 #-*- coding: utf-8 -*-
 
 import re
+import HTMLParser
 
+"""
 words = []
 pat = re.compile(r"(?:\\u[a-f0-9]{4})+")
 
@@ -20,3 +22,21 @@ words = [eval(u"u'%s'" % w) for w in words]
 with open('words', 'w') as f:
     for w in words:
         f.write(w.encode('utf-8')+"\n")
+
+"""
+
+with open("log2") as f:
+    html = f.read().strip()
+
+all_groups = re.findall("<div class=\"c\">(.*?)</div>(?=<div class=\"(?:[cs])\"\>)", html)
+all_groups = all_groups[2:-1]
+print len(all_groups)
+
+parser = HTMLParser.HTMLParser()
+
+group = all_groups[0]
+i = group
+people = re.findall(r'<span class="cmt">(.*?)</span>', i)[0]
+latest = re.findall(r'(?:</span>)(.*?)(?=<span)', i)[0]
+time = re.findall(r'<span class="ct">(.*?)</span>', i)[0]
+print people,latest,time
