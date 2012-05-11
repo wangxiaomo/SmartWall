d59de351d19550962e41756da930b921220d05c2
xiaomo <code.vvip@gmail.com>
2012-05-11 22:49:10 +0800

    get user info: name ,sex, school

--- Poster.py
+++ Poster.py
@@ -7,11 +7,15 @@ Poster: 发布微博。
 import sys
 sys.path.insert(0, "./lib")
 
+import re
 import pickle
+import urllib2
 import config
 from lib.weibopy.auth import OAuthHandler
 from lib.weibopy.api import API
 from Helper import log
+
+USER_URL_FORMAT = "http://weibo.cn/%s/info?vt=4&wm=ig_0001_index&gsid=%s"
 
 class Poster():
 
@@ -34,6 +38,11 @@ class Poster():
             print "Authorization URL: %s" % self.get_auth_url()
             pin = raw_input("PIN: ")
             self.auth(pin)
+        try:
+            with open(".gsid") as f:
+                self.gsid = f.read().rstrip()
+        except:
+            raise Exception("Haven't Got GSID!")
 
     def get_auth_url(self):
         return self.hdl.get_authorization_url()
@@ -58,16 +67,26 @@ class Poster():
 
     def get_user_info(self, screen_name):
         try:
+            user = {}
             ret = self.api.get_user(screen_name=screen_name)
-            ds = dir(ret)
-            r = ret.status
-            for w in dir(r):
-                if not w.startswith('_'):
-                    print r.__getattribute__(w)
+            user_id = ret.id
+            gender = ret.gender
+            url = USER_URL_FORMAT % (user_id, self.gsid)
+            request = urllib2.Request(url)
+            request.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:12.0) Gecko/20100101 Firefox/12.0')
+            html = urllib2.urlopen(request).read()
+            if re.match(r'哈尔滨工业大学|哈工大威海|哈工大（威海）|哈工大(威海)', html):
+                school = '哈工大'
+            elif re.match(r'山东大学|山大（威海）|山大(威海)', html):
+                school = '山大'
+            else:
+                school = '未知'
+            user.update(dict(id=user_id, name=screen_name, sex=gender, school=school))
+            print user
+            return user
         except:
-            raise Exception("%s user not find" % screen_name)
-            
+            raise
 
 if __name__ == '__main__':
     poster = Poster()
-    poster.get_user_info("小默的研究中心")
+    poster.get_user_info("梦逝去的方向")
